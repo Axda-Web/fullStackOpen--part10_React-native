01c177cda1659786cfcc2ecc87bf1b31ea73003e
Alyx DARENNE <axda.web@gmail.com>
2023-09-13 16:55:07 +0200

    10.8: the sign-in form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,19 @@
-import Text from "./Text";
+import SignInForm from "./SignInForm";
+import { Formik } from "formik";
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const onSubmit = ({ username, password }) => {
+    console.log({ username, password });
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
